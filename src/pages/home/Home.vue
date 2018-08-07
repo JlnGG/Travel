75e44dd417120ffa2fceed4722cbe3fbc23a218b
#G <1285447275@qq.com>
2018-08-07 17:57:27 +0800

    add header

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+	<Home-header></Home-header>
+</template>
+
+<script>
+import HomeHeader from './components/Header'
+export default {
+	name: "Home",
+	components: {
+		HomeHeader
+	}
+}
+</script>
+
+<style>
+	
+</style>
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="header">
+		<div class="header-left">返回</div>
+		<div class="header-input">输入城市/景点/游玩主题</div>
+		<div class="header-right">城市</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "HomeHeader"
+}
+</script>
+
+<style lang="stylus" scoped>
+	.header
+		height:.86rem
+		display:flex
+		background: #00bcd4
+		color: #fff
+		line-height: .86rem
+		.header-left
+			width: .64rem
+			float: left
+		.header-input
+			background: #fff
+			flex:1
+			margin-top: .12rem
+			margin-left: .2rem
+			height:.64rem
+			border-radius: .1rem
+			color: #ccc
+			line-height: .64rem
+			height: .64rem
+		.header-right	
+			width: 1.24rem
+			float: right
+			text-align: center
+</style>
