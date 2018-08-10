e88f806dcef8cda4e213eb2798e6f21ab53c3d57
#G <1285447275@qq.com>
2018-08-10 18:26:38 +0800

    city-list

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,18 +2,21 @@
   <div>
     <City-header></City-header>
     <City-search></City-search>
+    <City-list></City-list>
   </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
+import CityList from './components/List'
 
 export default {
   name: 'City',
   components: {
     CityHeader,
-    CitySearch
+    CitySearch,
+    CityList
   }
 }
 </script>
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="list">
+    <div class="area">
+      <div class="city">当前城市</div>
+      <div class="buttons-city">
+        <div class="button-city">北京</div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="city">热门城市</div>
+      <div class="buttons-city">
+        <div class="button-city">北京</div>
+      </div>
+      <div class="buttons-city">
+        <div class="button-city">北京</div>
+      </div>
+      <div class="buttons-city">
+        <div class="button-city">北京</div>
+      </div>
+      <div class="buttons-city">
+        <div class="button-city">北京</div>
+      </div>
+      <div class="buttons-city">
+        <div class="button-city">北京</div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="city">A</div>
+      <div class="city-list">
+        <ul class="city-ul">
+          <li class="city-li">北京</li>
+          <li class="city-li">北京</li>
+          <li class="city-li">北京</li>
+          <li class="city-li">北京</li>
+          <li class="city-li">北京</li>
+          <li class="city-li">北京</li>
+        </ul>
+      </div>
+    </div>
+    <div class="area">
+      <div class="city">B</div>
+      <div class="city-list">
+        <ul class="city-ul">
+          <li class="city-li">北京</li>
+          <li class="city-li">北京</li>
+          <li class="city-li">北京</li>
+          <li class="city-li">北京</li>
+          <li class="city-li">北京</li>
+          <li class="city-li">北京</li>
+          <li class="city-li">北京</li>
+        </ul>
+      </div>
+    </div>
+    <div class="area">
+      <div class="city">C</div>
+      <div class="city-list">
+        <ul class="city-ul">
+          <li class="city-li">北京</li>
+          <li class="city-li">北京</li>
+          <li class="city-li">北京</li>
+          <li class="city-li">北京</li>
+          <li class="city-li">北京</li>
+          <li class="city-li">北京</li>
+          <li class="city-li">北京</li>
+          <li class="city-li">北京</li>
+          <li class="city-li">北京</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityList'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~@/assets/styles/varstylus.styl'
+
+  .list{
+    position:absolute;
+    top: 1.58rem;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    overflow: hidden;
+    .area{
+      &:after{
+        content: '';
+        display: block;
+        clear: both;
+      }
+      .city{
+        line-height: .4rem;
+        background-color: #eee;
+        padding-left: .2rem;
+        color: #666;
+        font-size: .26rem;
+        border-top: .02rem solid #ccc;
+        border-bottom: .02rem solid #ccc;
+      }
+      .buttons-city{
+        overflow: hidden;
+        padding: .1rem 0 .1rem .1rem;
+        float:left;
+        width: 30%;
+        .button-city{
+          padding: .1rem;
+          box-sizing: border-box;
+          text-align: center;
+          margin: .1rem;
+          border: .02rem solid #ccc;
+          border-radius: .06rem;
+        }
+      }
+      .city-list{
+        .city-li{
+          line-height: .76rem;
+          padding-left: .2rem;
+          border-bottom: .02rem solid #ccc;
+        }
+      }
+    }
+  }
+</style>
